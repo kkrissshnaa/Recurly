f2470e9172d3fdf38b4408a8a4a3cfd17379b176
krishna <krishnabh0409@gmail.com>
2026-06-24 20:59:45 +0530

    Implemented insight go back

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,6 +1,7 @@
+import { Link } from "expo-router";
 import { styled } from "nativewind";
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, } from 'react-native';
 import { SafeAreaView as RNsafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNsafeAreaView);
@@ -9,6 +10,7 @@ const insights = () => {
   return (
    <SafeAreaView className="flex-1 bg-background p-5">
     <Text>Insights</Text>
+    <Link href={"/"}>Go back</Link>
    </SafeAreaView>
   )
 }
